02207d504a77c467b2338d1c138dd43d864b2f1f
Sanychcz <titov.cz@gmail.com>
2013-10-17 16:23:26 +0200

    main page styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
   def home
-    @categories = Category.all
+    @categories = Category.all 
+    @articles = Article.all
+    @products = Product.all
   end
 
   def contact
