f9c4b9308b0b10640f6d1d7f224071017956cd81
Sanychcz <titov.cz@gmail.com>
2013-10-26 17:46:58 +0200

    nav, logo fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   layout "application"
 
   def home
-    @categories = Category.includes( :photos).to_a 
+    @categories = Category.includes(:photos).to_a
     @articles = Article.recent.includes(:photos).to_a
     @products = Product.recent_products.includes(:photos).to_a
   end
