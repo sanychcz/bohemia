0b0e5a60a95052c4decb5390449928ab5cb4f424
Sanychcz <titov.cz@gmail.com>
2013-11-12 19:20:01 +0100

    added present

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,12 +1,2 @@
 module CategoriesHelper
-
-	def photo_for_category(category)
-		category.photos.each do |photo|
-		  if photo.present? 
-		  	image_tag photo.image_url(:cat_main_thumb).to_s
-		  else	 
-		  	image_tag "dummy_325x210.png"
-		  end 
-		end
-	end
 end
