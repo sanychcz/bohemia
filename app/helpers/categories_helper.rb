c5e2538ef18514d4d42c608bbe029ffdb227d458
Sanychcz <titov.cz@gmail.com>
2013-11-12 19:06:36 +0100

    added helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,8 @@
 module CategoriesHelper
+
+	def photo_for_category(cateory)
+		category.photos.each do |photo|
+		  photo.present? image_tag photo.image_url(:cat_main_thumb).to_s : image_tag "dummy_325x210.png"
+		end
+	end
 end
