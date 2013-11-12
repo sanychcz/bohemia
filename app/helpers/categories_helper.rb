cb0b227e956c266c719d8973d3bf81d54c0c5092
Sanychcz <titov.cz@gmail.com>
2013-11-12 19:15:08 +0100

    mistake in args

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,6 +1,6 @@
 module CategoriesHelper
 
-	def photo_for_category(cateory)
+	def photo_for_category(category)
 		category.photos.each do |photo|
 		  if photo.present? 
 		  	image_tag photo.image_url(:cat_main_thumb).to_s
