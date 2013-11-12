bb7a7a1a9adf26052007651138d3350d4b615c00
Sanychcz <titov.cz@gmail.com>
2013-11-12 19:08:20 +0100

    changed helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,7 +2,7 @@ module CategoriesHelper
 
 	def photo_for_category(cateory)
 		category.photos.each do |photo|
-		  photo.present? image_tag photo.image_url(:cat_main_thumb).to_s : image_tag "dummy_325x210.png"
+		  photo.present? image_tag(photo.image_url(:cat_main_thumb).to_s) : image_tag("dummy_325x210.png") 
 		end
 	end
 end
